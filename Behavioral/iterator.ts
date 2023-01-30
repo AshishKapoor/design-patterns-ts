f6d093f70658d331711750d7027ccc7383d84a8f
Ashish Kapoor <AshishKapoor@users.noreply.github.com>
2023-01-30 14:52:28 +0530

    Update iterator.ts

--- Behavioral/iterator.ts
+++ Behavioral/iterator.ts
@@ -1,4 +1,7 @@
-// The iterator pattern is used to traverse a collection of elements. Most programming languages provide abstrations for iteration like the for loop. However, you can create your own iterators in JavaScript by using the Symbol.iterator property. The code below creates a custom range function that can be used in a regular for loop.
+// The iterator pattern is used to traverse a collection of elements.
+// Most programming languages provide abstrations for iteration like the for loop.
+// However, you can create your own iterators in JavaScript by using the Symbol.iterator property.
+// The code below creates a custom range function that can be used in a regular for loop.
 
 function range(start: number, end: number, step=1) {
   return {
